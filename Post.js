f47093f9b8cacd66a51af76e85a1ae1d94b905b4
responsiblehemmher <128981688+responsiblehemmher@users.noreply.github.com>
2023-07-08 05:04:55 -0700

    Add files via upload

---
+++ Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="image">
+      <img src="./afrimusic2.jpg" alt=""/>
+      </div>
+        <div className="texts">
+        <h2>African Music will be Elevated, Trace TV Made this Emphasis During an Award Show and Festival</h2>
+        <p className="info">
+          <a className="myAuthor">Edunjobi Emmanuel</a>
+          <time>2023-07-05 14:20</time>
+        </p>
+        <p className="summary">African music is experiencing high-level cross-cultural pollination all throughout the continent, from the speaker-rattling Amapiano of Southern Africa to its boiling hybrid in West Africa down to the sonic richness of Eastern Africa and the diversity of North Africa.</p>
+        </div>
+      </div>
+  );
+}
